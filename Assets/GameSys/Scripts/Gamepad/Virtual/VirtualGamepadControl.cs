4aeba14898729c602022bba65fd64afead5be012
Aaron Malley <aaron.d.malley@gmail.com>
2019-03-14 21:51:44 -0300

    Adding virtual gamepad control to try to fix commit

--- Assets/GameSys/Scripts/Gamepad/Virtual/VirtualGamepadControl.cs
+++ Assets/GameSys/Scripts/Gamepad/Virtual/VirtualGamepadControl.cs
@@ -2,16 +2,5 @@
 
 namespace GameSys.Gamepad
 {
-    public abstract class VirtualGamepadControl : MonoBehaviour,
-        IGamepadControl
-    {
-        public event GamepadControlChangeEvent GamepadControlChange;
 
-        public virtual void Poll() { }
-
-        public void ControlChanged(AxisType axisType, float before, float after)
-        {
-            GamepadControlChange?.Invoke(axisType, before, after);
-        }
-    }
 }
